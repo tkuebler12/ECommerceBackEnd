fd137d14e23b100068c074bd0882ed87941ae581
Timothy Kuebler <timothy.j.kuebler@gmail.com>
2021-04-19 13:47:01 -0400

    added api routes

--- models/index.js
+++ models/index.js
@@ -7,29 +7,19 @@ const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
   through: {
-    model: Category,
+    model: Product,
     unique: false
   },
 
   as: `product_id`
 
 });
-// Categories have many Products
-
-Category.belongsToMany(Product, {
-  through: {
-    model: Product,
-    unique: false
-  },
-  
-  as: `category_product`
-});
 
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
   through: {
-    model: Tag,
+    model: ProductTag,
     unique: false
   },
 
